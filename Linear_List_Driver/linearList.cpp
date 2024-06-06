56f8c6058f617d4078517a35c33337fc65c2d831
ameerlouly <loulyameer@gmail.com>
2024-06-06 18:02:19 +0300

    Linear Lists

--- Linear_List_Driver/linearList.cpp
+++ Linear_List_Driver/linearList.cpp
@@ -13,3 +13,26 @@ void linearList::insert(const elemtype &e)
     listarray[number] = e;
     number++;
 }
+
+bool linearList::first(elemtype &e)
+{
+    if(number == 0)
+        return false;
+
+    current = 0;
+    e = listarray[current];
+
+    return true;
+}
+
+bool linearList::next(elemtype &e)
+{
+    assert(current >= 0);
+
+    if(current >= number - 1)
+        return false;
+
+    current++;
+    e = listarray[current];
+    return true;
+}
