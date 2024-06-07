314d3aa821f8d1c96920fcb09a8e3c929290d2a8
ameerlouly <loulyameer@gmail.com>
2024-06-07 10:35:37 +0300

    Made an edit on Q3 spring 2023

--- Spring 2023 Exam/q3.cpp
+++ Spring 2023 Exam/q3.cpp
@@ -6,10 +6,19 @@ bool linkedList::remove_bunch(int n, int m)
     link p, s;
     current = head;
     for(int i = 1; i < n; i++)
+    {
+        if(current->next == 0)   //Checks if i reached the end of the list, and returns false if true
+            return false;     
         current = current->next;
+    }
     p = current; //P points to the node before the node i want to start deleting from
+      
     for(int i = 1; i <= m; i++)
+    {
+        if(current->next == 0)  //Checks if i reached the end of the list, and returns false if true
+            return false;     
         current = current->next;
+    }
     s = current; //s points to the last node i want to delete
 
     current = p;  
